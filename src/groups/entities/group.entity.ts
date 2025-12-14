6df08d9f3504c5e52ecded43b44ff62c0f09a153
MITTERRAND Melina <melina.mitterrand@cimpa.com>
2025-12-14 13:05:46 +0100

    Ajout des entites TypeORM initiales

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,18 @@
-export class Group {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Group {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,34 @@
-export class Task {}
+import { Group } from 'src/groups/entities/group.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { CreateDateColumn } from 'typeorm/browser';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ default: 1 })
+  weight: number;
+
+  @Column({ default: 'ONCE' })
+  frequency: 'ONCE' | 'DAILY' | 'WEEKLY';
+
+  @Column({ nullable: true })
+  dueDate: Date;
+
+  @ManyToOne(() => Group)
+  group: Group;
+
+  @ManyToOne(() => User)
+  createdBy: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
