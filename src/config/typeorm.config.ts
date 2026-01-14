bcb50561dec58729f0d74465ee1d83d1d31658b6
melinaaaa0805 <melina.mitterrand08@gmail.com>
2026-01-14 22:09:32 +0100

    modif

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,11 +8,11 @@ import { ShoppingList } from '../shopping-list/entities/shopping-list.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'sharelife_db',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   autoLoadEntities: true,
   entities: [User, Group, GroupMember, Task, TaskAssignment, ShoppingList],
   synchronize: true, // seulement pour dev
